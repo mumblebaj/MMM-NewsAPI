917d571adb6c020479f830ee6bbc817b07953c22
mumblebaj <bernardmumble@gmail.com>
2021-08-04 21:01:40 +0200

    rewrite all

---
+++ MMM-NewsAPI.js
@@ -0,0 +1,169 @@
+Module.register("MMM-NewsAPI", {
+    // Declare default inputs
+    defaults: {
+        apiKey: "",
+		type: "horizontal",
+		choice: "headlines",
+		pageSize: 20,
+		timeFormat: "relative",
+		drawInterval: 1000*30,
+        fetchInterval: 1000*60*60,
+		templateFile: "template.html",
+        query: {
+            country: "us",
+            category: "",
+            q: "",
+            qInTitle: "",
+            sources: "",
+            domains: "",
+            excludeDomains: "",
+            from: "",
+            to: "",
+            language: "en",
+            sortBy: ""
+        }
+    },
+
+    // Get the Stylesheet
+    getStles: function() {
+        return["MMM-NewsAPI.css"]
+    },
+
+    // Declare any translations
+    // getTranslations: function() {
+    //     return {
+    //         en: "translations/en.json",
+    //         de: "translations/de.json",
+    //         fr: "translations/fr.json",
+    //     }
+    // },
+
+    // Start process
+    start: function() {
+        this.firstUpdate = 0
+        this.index = 0
+        this.timer = null
+        this.template = ""
+        this.newsArticles = []
+        // Start function call to node_helper
+        this.getInfo()
+        // Schedule the next update
+        this.scheduleUpdate()
+    },
+
+    getDom: function() {
+        var wrapper = document.createElement("div")
+        wrapper.id = "NEWS"
+        wrapper.className = this.config.type
+        var newsContent = document.createElement("div")
+        newsContent.id = "NEWS_CONTENT"
+        wrapper.appendChild(newsContent)
+        wrapper.classList.add("untouchable")
+        return wrapper
+    },
+
+    notificationReceived: function(noti, payload) {
+        switch (noti) {
+            case "DOM_OBJECTS_CREATED":
+                this.readTemplate()
+                this.sendSocketNotification("START")
+                break
+        }
+    },
+
+    // Schedule the next update
+    scheduleUpdate: function(delay) {
+        var nextLoad = this.config.fetchInterval
+        if (typeof delay !== "undefined"  && delay >= 0) {
+            nextLoad = delay
+        }
+        console.log("Delay till next load", delay)
+        var self = this
+        setInterval(function() {
+            self.getInfo()
+        }, nextLoad)
+    },
+
+    // Send Socket Notification and start node_helper
+    getInfo: function() {
+        if (this.config.choice === "headlines"){
+            this.sendSocketNotification("headlines", this.config)
+        } else if (this.config.choice === "everything") {
+            this.sendSocketNotification("everything", this.config)
+        } else { 
+            console.log("NewsAPI: Invalid choice defined in COnfig") 
+            return true
+        }
+    },
+
+    // Receive Socket Notification
+    socketNotificationReceived: function(notification, payload) {
+        var self = this
+        if (notification === "UPDATE") {
+            this.newsArticles = payload
+            if (this.firstUpdate == 0) {
+                this.firstUpdate = 1
+                this.index = 0
+                this.draw()
+            }
+        }
+    },
+
+    readTemplate: function() {
+        var file = this.config.templateFile
+        var url = "modules/MMM-NewsAPI/" + file
+        var xmlHttp = new XMLHttpRequest()
+        xmlHttp.onreadystatechange = () => {
+            var res = []
+            if (xmlHttp.readyState == 4 && xmlHttp.status == 200) this.template = xmlHttp.responseText
+            else if (xmlHttp.status !== 200 && xmlHttp.readyState !== 1) {
+                console.log("A Problem has been encountered retrieving the Template FIle", "("+xmlHttp.statusText+")")
+            }
+        }
+        xmlHttp.open("GET", url, true)
+        xmlHttp.send()
+    },
+
+    draw: function() {
+        clearTimeout(this.timer)
+        this.timer = null
+        const tag = [
+            "sourceId", "author", "content", "description", "articleId",
+            "sourceName", "title", "url", "urlToImage", "publishedAt"
+        ]
+        var article = this.newsArticles[this.index]
+        var template = this.template
+
+        for (i in tag) {
+            var t = tag[i]
+            var tu = "%" + t.toUpperCase() + "%"
+            template = template.replace(tu, article[t])
+        }
+
+        var imgtag = (article.urlToImage) ? `<img class="articleImage" src="` + article.urlToImage + `"/>` : ""
+        template = template.replace("%ARTICLEIMAGE%", imgtag)
+        var className = (redTitle) ? "" : ""
+        template = template.replace("%CLASSNAME%", className)
+        template = template.replace("%AUHTOR%", article.author)
+        var news = document.getElementById("NEWS")
+
+        var newsContent = document.getElementById("NEWS_CONTENT")
+        news.classList.add("hideArticle")
+        news.classList.remove("showArticle")
+        for (j in article) news.dataset[j] = article[j]
+
+        setTimeout(() => {
+            newsContent.innerHTML = ""
+            news.classList.remove("hideArticle")
+            news.classList.add("showArticle")
+            newsContent.innerHTML = template
+
+        }, 900)
+        this.timer = setTimeout(() => {
+            this.index++
+            if (this.index >= this.newsArticles.length) this.index = 0
+            this.draw()
+        }, this.config.drawInterval)
+    }
+
+})
---
+++ node_helper.js
@@ -0,0 +1,114 @@
+// Imports
+var NodeHelper = require('node_helper')
+var request = require('request')
+var moment = require('moment')
+const querystring = require('querystring')
+
+// Any declarations
+
+module.exports = NodeHelper.create({
+    // Start function
+    start: function() {
+        console.log('Starting node_helper for module: ' + this.name)
+
+        // Declare any defaults
+        this.config = null
+        this.articles = []
+        this.endPoint1 = "https://newsapi.org/v2/top-headlines?"
+        this.endPoint2 = "https://newsapi.org/v2/everything?"
+    },
+
+    deconHeadlines: function(payload) {
+        var query = payload
+        for(i in query) {
+            var q = query[i]
+            var qs = {}
+            if (q.hasOwnProperty("sources") && q.sources !== "") {
+                var t = q["sources"].replace(/\s/g, "")
+                qs = Object.assign({}, qs, {"sources":t})
+            }
+            if (q.hasOwnProperty("q") && q.q !== "") qs = Object.assign({}, qs, {"q":q["q"]})
+            if (q.hasOwnProperty("qInTitle") && q.qInTitle !== "") qs = Object.assign({}, qs, {"qIntTitle":q["qInTitle"]})
+            if (q.hasOwnProperty("domains") && q.domains !== "") {
+                var d = q["domains"].replace(/\s/g, "")
+                qs = Object.assign({}, qs, {"domains":d})
+            }
+            if (q.hasOwnProperty("excludeDomains") && q.excludeDomains !== "") {
+                var ed = q["domains"].replace(/\s/g, "")
+                qs = Object.assign({}, qs, {"excludeDomains":ed})
+            }
+            if (q.hasOwnProperty("language") && q.language !== "") qs = Object.assign({}, qs, {"language":q["language"]})
+            if (q.hasOwnProperty("country") && q.country !== "" && q.sources === "") qs = Object.assign({}, qs, {"country":q["country"]})
+            qs = Object.assign({}, qs, {"pageSize":payload.pageSize})
+            qs = Object.assign({}, qs, {"apiKey":payload.apiKey})
+            var qp = querystring.stringify(qs)
+            var callScript = this.endPoint1 + qp
+        }
+        this.getArticles(callScript, payload)
+    },
+
+    deconEverything: function(payload){
+        var query = payload
+        for(i in query) {
+            var q = query[i]
+            var qs = {}
+            if (q.hasOwnProperty("q") && q.q !== "") qs =Object.assign({}, qs, {"q":q["q"]})
+            if (q.hasOwnProperty("qInTitle") && q.qInTitle !== "") qs = Object.assign({}, qs, {"qIntTitle":q["qInTitle"]})
+            if (q.hasOwnProperty("domains") && q.domains !== "") {
+            var d = q["domains"].replace(/\s/g, "")
+            qs = Object.assign({}, qs, {"domains":d})
+            }
+            if (q.hasOwnProperty("excludeDomains") && q.excludeDomains !== "") {
+                var ed = q["excludeDoomains"].replace(/\s/g, "")
+                qs = Object.assign({}, qs, {"excludeDomains":ed})
+            }
+            if (q.hasOwnProperty("sources") && q.sources !== "") {
+            var t = q["sources"].replace(/\s/g, "")
+            qs = Object.assign({}, qs, {"sources":t})
+            }
+            if (q.hasOwnProperty("language") && q.language !== "") qs = Object.assign({}, qs, {"language":q["language"]})
+            qs = Object.assign({}, qs, {"pageSize":payload.pageSize})
+            qs = Object.assign({}, qs, {"apiKey":payload.apiKey})
+            var qp = querystring.stringify(qs)
+            var callScript = this.endPoint2 + qp
+        }
+        this.getArticles(callScript, payload)
+    },
+
+    formatResults: function(ret, payload) {
+        var results = []
+        var self = this
+        var count = payload.pageSize
+        for (j in ret.articles) {
+            var article = ret.articles[j]
+            article.sourceName = article.source.name
+            if(!article.content) article.content = article.description
+            if(!article.author) article.author = ""
+            if(!article.description) article.description = article.content
+            results.push(article)
+        }
+        if (results.length > 0) this.articles = this.articles.concat(result)
+        this.sendSocketNotification("UPDATE", this.articles)
+    },
+
+    getArticles: function(callScript, payload) {
+        var callscript = callScript
+        request (callscript, (error, response, body) => {
+            if (!error && response.statusCode == 200) {
+                var ret = JSON.parse(body)
+                this.formatResults(ret, payload)
+            } else { console.log("Error retrieving data: ", error) }
+        })
+    },
+
+    // Socket Notification Received
+    socketNotificationReceived: function(notification, payload) {
+        if (notification === "headlines") {
+            this.deconHeadlines(payload)
+        } else if (notification === "everything") {
+            this.deconEverything(payload)
+        } else {
+            console.log("MMM-NewsAPI: NO valid choice received")
+        }
+    },
+})
