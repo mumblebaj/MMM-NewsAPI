e0afb14f6c29d3cefccf651c3f1490ff3313739a
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2024-09-20 22:32:18 +0200

    Handle ESLint issues

--- MMM-NewsAPI.js
+++ MMM-NewsAPI.js
@@ -1,3 +1,4 @@
+/* global Log Module QRious */
 Module.register("MMM-NewsAPI", {
     // Declare default inputs
     defaults: {
@@ -26,47 +27,46 @@ Module.register("MMM-NewsAPI", {
     },
 
     // Get the Stylesheet
-    getStyles: function () {
+    getStyles () {
         return [this.file("MMM-NewsAPI.css")]
     },
 
     // Import QR code script file
-    getScripts: function () {
+    getScripts () {
         return [this.file("node_modules/qrious/dist/qrious.min.js")]
     },
 
     // Start process
-    start: function () {
+    start () {
         this.firstUpdate = 0
         this.index = 0
         this.timer = null
         this.template = ""
-        suspended = false;
         this.newsArticles = []
-        if (this.config.debug) Log.log("config: ", JSON.stringify(this.config))
+        if (this.config.debug) { Log.log("config: ", JSON.stringify(this.config)) }
         // Start function call to node_helper
         this.getInfo()
         // Schedule the next update
         this.scheduleUpdate()
     },
 
-    stop: function () {
-        Log.info('Stopping module ' + this.name);
+    stop () {
+        Log.info(`Stopping module ${this.name}`)
     },
 
-    getDom: function () {
-        var wrapper = document.createElement("div")
+    getDom () {
+        const wrapper = document.createElement("div")
         wrapper.id = "NEWSAPI"
         wrapper.className = this.config.type
-        var newsContent = document.createElement("div")
+        const newsContent = document.createElement("div")
         newsContent.id = "NEWS_CONTENT"
         wrapper.appendChild(newsContent)
         wrapper.classList.add("untouchable")
         return wrapper
     },
 
-    notificationReceived: function (noti, payload) {
-        switch (noti) {
+    notificationReceived (notification) {
+        switch (notification) {
             case "DOM_OBJECTS_CREATED":
                 this.readTemplate()
                 this.sendSocketNotification("START")
@@ -75,39 +75,38 @@ Module.register("MMM-NewsAPI", {
     },
 
     // Schedule the next update
-    scheduleUpdate: function (delay) {
-        if (this.config.debug) Log.log("Fetch Interval: ", this.config.fetchInterval)
-        var nextLoad = this.config.fetchInterval
+    scheduleUpdate (delay) {
+        if (this.config.debug) { Log.log("Fetch Interval: ", this.config.fetchInterval) }
+        let nextLoad = this.config.fetchInterval
         if (typeof delay !== "undefined" && delay >= 0) {
             nextLoad = delay
         }
-        var self = this
-        setInterval(function () {
-            //if (this.config.debug) Log.log("getting the next batch of data")
+        const self = this
+        setInterval(() => {
+            Log.debug("getting the next batch of data")
             self.getInfo()
         }, nextLoad)
     },
 
     // Send Socket Notification and start node_helper
-    getInfo: function () {
-        if (this.config.debug) Log.log("selected choice: ", this.config.choice)
+    getInfo () {
+        if (this.config.debug) { Log.log("selected choice: ", this.config.choice) }
         if (this.config.choice === "headlines") {
             this.sendSocketNotification("headlines", this.config)
         } else if (this.config.choice === "everything") {
             this.sendSocketNotification("everything", this.config)
         } else {
-            console.log("NewsAPI: Invalid choice defined in config/config.js. Use 'headlines' or 'everything'")
+            Log.log("NewsAPI: Invalid choice defined in config/config.js. Use 'headlines' or 'everything'")
             return true
         }
     },
 
     // Receive Socket Notification
-    socketNotificationReceived: function (notification, payload) {
-        if (this.config.debug) Log.log("payload received: ", JSON.stringify(payload))
-        var self = this
+    socketNotificationReceived (notification, payload) {
+        if (this.config.debug) { Log.log("payload received: ", JSON.stringify(payload)) }
         if (notification === "NEWSAPI_UPDATE") {
             this.newsArticles = payload
-            if (this.firstUpdate == 0) {
+            if (this.firstUpdate === 0) {
                 this.firstUpdate = 1
                 this.index = 0
                 this.draw()
@@ -115,47 +114,46 @@ Module.register("MMM-NewsAPI", {
         }
     },
 
-    async readTemplate() {
-        const file = this.config.templateFile;
-        const url = `modules/MMM-NewsAPI/${file}`;
+    async readTemplate () {
+        const file = this.config.templateFile
+        const url = `modules/MMM-NewsAPI/${file}`
         try {
-            const response = await fetch(url);
+            const response = await fetch(url)
             if (!response.ok) {
-                throw new Error(`A Problem has been encountered retrieving the Template File (${response.statusText})`);
+                throw new Error(`A Problem has been encountered retrieving the Template File (${response.statusText})`)
             }
-            this.template = await response.text();
+            this.template = await response.text()
         } catch (error) {
-            console.error(error.message);
+            Log.error(error.message)
         }
     },
 
-    draw: function () {
+    draw () {
         clearTimeout(this.timer)
         this.timer = null
         const tag = [
             "sourceId", "author", "content", "description", "articleId",
             "sourceName", "title", "url", "urlToImage", "publishedAt"
         ]
-        var article = this.newsArticles[this.index]
-        var template = this.template
+        const article = this.newsArticles[this.index]
+        let template = this.template
 
-        for (i in tag) {
-            var t = tag[i]
-            var tu = "%" + t.toUpperCase() + "%"
+        for (const i in tag) {
+            const t = tag[i]
+            const tu = `%${t.toUpperCase()}%`
             template = template.replace(tu, article[t])
         }
 
-        var imgtag = (article.urlToImage) ? `<img class="articleImage" src="` + article.urlToImage + `"/>` : ""
+        const imgtag = article.urlToImage ? `<img class="articleImage" src="${article.urlToImage}"/>` : ""
         template = template.replace("%ARTICLEIMAGE%", imgtag)
-        var className = (article.className) ? article.className : ""
-        template = template.replace("%CLASSNAME%", 'NEWSAPI') //"NEWS"
+        template = template.replace("%CLASSNAME%", "NEWSAPI") // "NEWS"
         template = template.replace("%AUTHOR%", article.author)
-        var news = document.getElementById("NEWSAPI")
+        const news = document.getElementById("NEWSAPI")
 
-        var newsContent = document.getElementById("NEWS_CONTENT")
+        const newsContent = document.getElementById("NEWS_CONTENT")
         news.classList.add("hideArticle")
         news.classList.remove("showArticle")
-        for (j in article) news.dataset[j] = article[j]
+        for (const j in article) { news.dataset[j] = article[j] }
 
         setTimeout(() => {
             newsContent.innerHTML = ""
@@ -163,22 +161,22 @@ Module.register("MMM-NewsAPI", {
             news.classList.add("showArticle")
             newsContent.innerHTML = template
             if (this.config.QRCode) {
-                var qr = new QRious({
-                    element: document.getElementById('NEWSAPI_QRCODE'),
+                const qr = new QRious({
+                    element: document.getElementById("NEWSAPI_QRCODE"),
                     value: article.url
-                });
+                })
             } else {
-                var qrCodeElement = document.getElementById('NEWSAPI_QRCODE');
+                const qrCodeElement = document.getElementById("NEWSAPI_QRCODE")
                 if (qrCodeElement) {
-                    qrCodeElement.parentNode.removeChild(qrCodeElement);
+                    qrCodeElement.parentNode.removeChild(qrCodeElement)
                 }
             }
         }, 900)
         this.timer = setTimeout(() => {
             this.index++
-            if (this.index >= this.newsArticles.length) this.index = 0
+            if (this.index >= this.newsArticles.length) { this.index = 0 }
             this.draw()
         }, this.config.drawInterval)
     }
-
 })
+
